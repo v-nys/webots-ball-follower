0c17d2cf9ea69ed445b59e546a3e5c98849025cd
Vincent Nys <vincentnys@gmail.com>
2025-11-09 17:47:42 +0100

    Figured out tricky bits.

--- controllers/epuck_approach_color/epuck_approach_color.py
+++ controllers/epuck_approach_color/epuck_approach_color.py
@@ -18,37 +18,34 @@ class RobotState(Enum):
     ARRIVED = 4  # niets meer te doen
 
 
-HUE_VAL = 0  # 0 is red
-# HSV must all be at least this (on scale 360/255/255)
-# but opencv color conversion produces H value between 0 and 180...
-LOWER_COLOR = np.array([(HUE_VAL - 10) % 360, 100, 100])
-UPPER_COLOR = np.array([(HUE_VAL + 10) % 360, 255, 255])
-# zie https://cyberbotics.com/doc/reference/camera: 32 bits BGRA
-RED_BGRA_PIXEL = bytes.fromhex("0000ffff")
-# GREEN_BGRA_PIXEL = bytes.fromhex("00ff00ff")
-# YELLOW_BGRA_PIXEL = bytes.fromhex("00f7ffff")
-# BLUE_BGRA_PIXEL = bytes.fromhex("ff0000ff")
-# MAGENTA_BGRA_PIXEL = bytes.fromhex("ff00ffff")
-NEARRED_BGRA_PIXEL = bytes.fromhex("1000ffff")
-IMG_WIDTH, IMG_HEIGHT = (4, 3)  # oorpsronkelijk 320x240
-
-# 0 hue is red
-# 30-ish is yellow
-# 60-ish is green
-# 120 is blue
+# 0 hue is rood
+# 30 is geel
+# 60-ish is groen
+# 90 is appelblauw
+# 120 is blauw
 # 150 is magenta
-# so 180 will be red again
-# but this seems inconsistent with docs
-
+# 180 is terug rood
+HUE_VAL = 0
+assert 0 <= HUE_VAL < 180
+HUE_OFFSET = 15
+assert HUE_OFFSET >= 0 and HUE_OFFSET < 180, "geen zinvolle offsethoek"
+# geen modulo 180, want dan zou inRange (zie beneden) niet werken
+# zullen gecontroleerde hues moeten aanpassen
+LOWER_COLOR = np.array([(HUE_VAL - HUE_OFFSET), 50, 50])
+UPPER_COLOR = np.array([(HUE_VAL + HUE_OFFSET), 255, 255])
+# zie https://cyberbotics.com/doc/reference/camera: 32 bits totaal
+# dus "8-bit" afbeelding (see https://www.tourboxtech.com/en/news/bit-depth-explained.html)
+RED_BGRA_PIXEL = bytes.fromhex("0000ffff")
+GREEN_BGRA_PIXEL = bytes.fromhex("00ff00ff")
+IMG_WIDTH, IMG_HEIGHT = (320, 240)  # (4, 3)  # oorpsronkelijk 320x240 genomen
 TIME_STEP = 64
 robot = Robot()
 robot.state = RobotState.ORIENTING
-print(Path.cwd())
 
 camera = robot.getDevice("camera")
 leftMotor = robot.getDevice("left wheel motor")
 rightMotor = robot.getDevice("right wheel motor")
-# wat ook kan: gewoon vanaf start programma bijhouden vanaf 0
+# wat ook kan: gewoon vanaf start programma tel bijhouden
 leftPositionSensor = robot.getDevice("left wheel sensor")
 leftPositionSensor.enable(TIME_STEP)
 rightPositionSensor = robot.getDevice("right wheel sensor")
@@ -56,32 +53,27 @@ rightPositionSensor.enable(TIME_STEP)
 camera.enable(TIME_STEP)
 
 
-def check_rgba_values(arr):
-    plt.imshow(arr)
-    plt.axis("off")
-    plt.show()
-
-
 def decide_state(current_state, image_bytes):
-    # reference material assumes https://picamera.readthedocs.io/en/release-1.13/api_array.html#pirgbarray
-    # denk dat r, g en b worden voorgesteld als bytes, niet als floats
+    # boek gebruikt https://picamera.readthedocs.io/en/release-1.13/api_array.html#pirgbarray
+    # staat eigenlijk niet hoe veel bits per kleurkanaal er dan zijn, vermoedelijk 8
     numpy_1_dim_bgra_byte_array = np.frombuffer(image_bytes, dtype=np.uint8)
     numpy_3_dim_bgra_byte_array = np.reshape(
         numpy_1_dim_bgra_byte_array, shape=(IMG_HEIGHT, IMG_WIDTH, 4), order="C"
     )
     numpy_3_dim_rgba_byte_array = numpy_3_dim_bgra_byte_array[..., [2, 1, 0, 3]]
-    # check_rgba_values(numpy_3_dim_rgba_byte_array)
 
     # zie https://docs.opencv.org/3.4/d8/d01/group__imgproc__color__conversions.html
-    # met 32-bit image zouden H, S en V al in juiste range moeten zijn
+    # met 8-bit afbeelding en COLOR_RGB2HSV zou H tussen 0 en 180 moeten liggen
     hsv = cv2.cvtColor(numpy_3_dim_rgba_byte_array[..., :3], cv2.COLOR_RGB2HSV)
-    restored = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    check_rgba_values(restored)
-
-    # dus print misschien heel hsv[...,[0]]?
-    # dat zou een 4x3 moeten zijn met 3/4 groene hue en 1/4 rode
-    print(hsv[..., [0]])
-
+    h = hsv[..., 0]
+    # schuif waarden die niet vergelijkbaar zijn met grens op in de richting van grens
+    shift_down_mask = np.abs(h - LOWER_COLOR[0]) >= 180
+    # analoge redenering
+    shift_up_mask = np.abs(h - UPPER_COLOR[0]) >= 180
+    h_new = h.copy()
+    h_new[shift_down_mask] = h_new[shift_down_mask] - 180
+    h_new[shift_up_mask] = h_new[shift_up_mask] + 180
+    hsv[..., 0] = h_new
     color_mask = cv2.inRange(hsv, LOWER_COLOR, UPPER_COLOR)
     contours, _ = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     object_area = 0
@@ -98,12 +90,14 @@ def decide_state(current_state, image_bytes):
             object_y = center_y
     if object_area > 0:
         ball_location = [object_area, object_x, object_y]
-        print("Ik zie hem!")
+        print(f"Ik zie hem op x-positie {ball_location[1]}!")
     else:
         ball_location = None
         print("Ik zie hem niet!")
-    # TODO: zoek grootste rode cluster in gezichtsveld
-    # TODO: bepaal of die meer centraal moet
+    # TODO: bepaal wat er moet gebeuren
+    # indien we aan het oriënteren zijn, moet hij op IMG_WIDTH+-FOCUS_MARGIN komen
+    # indien we een het naderen zijn, mag hij wat off-center zijn, bv. IMG_WIDTH+-2FOCUS_MARGIN
+    # indien object_area heel groot is, zitten we er tegen...
     match current_state:
         case RobotState.ARRIVED:
             return RobotState.ARRIVED
@@ -116,14 +110,9 @@ def decide_state(current_state, image_bytes):
 
 
 while robot.step(TIME_STEP) != -1:
+    # "real" want heb dit ook getest met manueel aangemaakte "fake" bytes
     real_bytes = camera.getImage()
-    # 3/4 groen en 1/4 rood, kan alvast checken dat verhouding klopt
-    # formaat van camera.getImage zou BGRA (32 bits) moeten zijn
-    # zie https://cyberbotics.com/doc/reference/camera
-    fake_bytes = (NEARRED_BGRA_PIXEL * (IMG_WIDTH * IMG_HEIGHT * 3 // 4)) + (
-        RED_BGRA_PIXEL * (IMG_WIDTH * IMG_HEIGHT // 4)
-    )
-    robot.state = decide_state(robot.state, fake_bytes)
+    robot.state = decide_state(robot.state, real_bytes)
     match robot.state:
         case RobotState.ORIENTING:
             pass
