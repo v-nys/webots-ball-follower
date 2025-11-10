1442dde0e3c5441c674084939b254c6bed8c2e18
Vincent Nys <vincentnys@gmail.com>
2025-11-10 16:15:20 +0100

    Take into account missing theta.

--- controllers/epuck_gamepad_control/epuck_gamepad_control.py
+++ controllers/epuck_gamepad_control/epuck_gamepad_control.py
@@ -13,10 +13,12 @@ leftPositionSensor.enable(TIME_STEP)
 rightPositionSensor = robot.getDevice("right wheel sensor")
 rightPositionSensor.enable(TIME_STEP)
 EPSILON = 0.0001
-MIN_R_FOR_MOVEMENT = 0.5 # stick moet al wat ingedrukt zijn
+MIN_R_FOR_MOVEMENT = 0.5  # stick moet al wat ingedrukt zijn
 
 if len(sys.argv) != 2:
-    print("Er moet exact één argument zijn, namelijk het devicenummer van de gamepad. Check dat via `evtest`.")
+    print(
+        "Er moet exact één argument zijn, namelijk het devicenummer van de gamepad. Check dat via `evtest`."
+    )
     sys.exit(1)
 
 device_number = int(sys.argv[1])
@@ -27,8 +29,12 @@ while robot.step(TIME_STEP) != -1:
     abs_info_axis_1 = device.absinfo(1).value
     # krijgen horizontale/verticale waarde tussen -1 en 1
     # waarden stick zijn op voorhand gecontroleerd
-    horizontal = max(min(abs_info_axis_0 / (32768.0 if abs_info_axis_0 < 0 else 32767.0),1), -1)
-    vertical = -max(min(abs_info_axis_1 / (32768.0 if abs_info_axis_1 < 0 else 32767.0), 1), -1)
+    horizontal = max(
+        min(abs_info_axis_0 / (32768.0 if abs_info_axis_0 < 0 else 32767.0), 1), -1
+    )
+    vertical = -max(
+        min(abs_info_axis_1 / (32768.0 if abs_info_axis_1 < 0 else 32767.0), 1), -1
+    )
     r = math.sqrt(horizontal**2 + vertical**2)
     theta = None
     if r >= MIN_R_FOR_MOVEMENT:
@@ -43,11 +49,13 @@ while robot.step(TIME_STEP) != -1:
             theta = math.atan2(vertical, horizontal)
             if theta < 0:
                 theta += math.pi * 2
-        # ik ben nog niet helemaal zeker hoe goed dit zal werken met een echte stappenmotor
-        adjustment_left = math.sin(theta + math.pi / 4)
-        adjustment_right = math.sin(theta - math.pi / 4)
-        leftMotor.setPosition(leftPositionSensor.getValue() + adjustment_left)
-        rightMotor.setPosition(rightPositionSensor.getValue() + adjustment_right)
+        if theta:
+            # ik ben nog niet helemaal zeker hoe goed dit zal werken met een echte stappenmotor
+            # kan die met fracties van stappen werken?
+            adjustment_left = math.sin(theta + math.pi / 4)
+            adjustment_right = math.sin(theta - math.pi / 4)
+            leftMotor.setPosition(leftPositionSensor.getValue() + adjustment_left)
+            rightMotor.setPosition(rightPositionSensor.getValue() + adjustment_right)
     else:
         leftMotor.setPosition(leftPositionSensor.getValue())
         rightMotor.setPosition(rightPositionSensor.getValue())
